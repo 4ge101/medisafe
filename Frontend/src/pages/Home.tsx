b8ff7bf46604c94cdb42feddbcb9d68550e361bc
Ali Sami <theforgettenone2@gmail.com>
2026-04-28 23:40:02 +0545

    making home css

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import './Home.css';
+import '../styles/Home.css';
 
 function Home() {
   const navigate = useNavigate();
